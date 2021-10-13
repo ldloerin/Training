20215744f5b016b318abbdaac8e9a7f44714560b
David Loerinci <david.loerinci@gmail.com>
2021-10-13 10:55:35 +0200

    cleaning

--- HumanResourcesInheritance/productivity.py
+++ HumanResourcesInheritance/productivity.py
@@ -7,17 +7,21 @@ class ProductivitySystem:
             print(f'{employee.name}: {result}')
         print('')
 
+
 class ManagerRole:
     def work(self, hours):
         return f'screams and yells for {hours} hours.'
+
 
 class SecretaryRole:
     def work(self, hours):
         return f'expends {hours} hours doing office paperwork.'
 
+
 class SalesRole:
     def work(self, hours):
         return f'expends {hours} hours on the phone.'
+
 
 class FactoryRole:
     def work(self, hours):
